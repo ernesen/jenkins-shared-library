df4d3b7f62ae021b449833a9e88f01a264be2438
Ernese Norelus <ernese@sg.ibm.com>
2020-06-21 13:43:41 +0800

    Update getAppName.groovy

--- vars/getAppName.groovy
+++ vars/getAppName.groovy
@@ -3,8 +3,8 @@
  * @param repositoryUrl to app name
  */
 def call(repositoryUrl){
-    tmp = repositoryUrl.split('//')
-    tmp = tmp[1].split('/')
-    tmp =  tmp[2]
-    return tmp - ".git"
+    //tmp = repositoryUrl.split('//')
+    //tmp = tmp[1].split('/')
+    //tmp =  tmp[2]
+    return repositoryUrl - ".git"
 }
